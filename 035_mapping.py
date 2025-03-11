a44b8e765e92c07d79ee9414cf0764bbe1fcda12
Tawanda Chabva <tchabva@gmail.com>
2025-03-11 14:26:26 +0000

    complete the file tasksh

--- 035_mapping.py
+++ 035_mapping.py
@@ -35,7 +35,10 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
-  pass
+  plus_one_hundred = []
+  for n in numbers:
+    plus_one_hundred.append(n + 100)
+  return plus_one_hundred
 
 check_that_these_are_equal(
   add_one_hundred_to_numbers([1, 2, 3, 4]), [101, 102, 103, 104])
